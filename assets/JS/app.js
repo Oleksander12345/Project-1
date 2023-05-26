edcf137c3685b11e54eaa0b6239664d5d1673b53
Oleksander12345 <salisnyak07@gmail.com>
2023-05-26 12:26:43 +0300

    final

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -39,11 +39,19 @@ $(function () {
     });
 
     // Collapse
-    $("[data-scroll]").on("click", function (event) {
+    $("[data-collapse]").on("click", function (event) {
         event.preventDefault();
         var $this = $(this),
             blockId = $this.data('collapse');
-        $(blockId).slideToggle();
+        $this.toggleClass("active"); 
+    });
+
+    // Slider
+    $("[dat-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
     });
 
 
